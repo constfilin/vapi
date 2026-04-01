9b33b9a543d56182f7928011ee7c003bafd8c894
Constantine Filin <constfilin@gmail.com>
2026-04-01 12:52:43 -0700

    [#0]: added cmd line commands to manage phone numbers

--- getCmdPromise.ts
+++ getCmdPromise.ts
@@ -32,6 +32,10 @@ export const getCmdPromise = ( args:Record<string,any> ) => {
             args.sessionId   as string,
             args.question as string
         ));
+    case 'listPhoneNumbers':
+        return (() => vapiApi.phoneNumbers.list());
+    case 'deletePhoneNumber':
+        return (() => vapiApi.phoneNumbers.delete({id:args.id}));
     case 'getToolById':
         return (() => tools.get({id:args.id}));
     case 'getTool':
